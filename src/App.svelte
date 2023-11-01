ad3b04c2b244d2df20f3505c282ccde0a4bbd15a
Ruben Jimenez <rdja06@gmail.com>
2023-10-31 21:16:39 -0400

    First commit

---
+++ src/App.svelte
@@ -0,0 +1,27 @@
+<script>
+  import followUpAppLogo from '././assets/entrega.svg'
+</script>
+
+<header>
+  <div>
+    <img src={followUpAppLogo} class="Follow-up-App Logo" alt="Follow-up App Logo" />
+    <p><strong>Follow-up App</strong></p>
+</div>
+</header>
+
+<nav>
+  <li>Tasks</li>
+  <li>Reminders</li>
+</nav>
+
+
+<style>
+.Follow-up-App {
+  height: 50px;
+}
+
+
+
+
+
+</style>
